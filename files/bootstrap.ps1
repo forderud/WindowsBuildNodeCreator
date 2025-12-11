06415ff4371ea9588d5da1dcb1b69698f0965f99
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-12-11 01:01:11 +0100

    fix function body indentation

--- files/bootstrap.ps1
+++ files/bootstrap.ps1
@@ -19,25 +19,26 @@ $connections |ForEach-Object {
 }
 
 Function Enable-WinRM {
-Write-Host "Enable WinRM"
-netsh advfirewall firewall set rule group="remote administration" new enable=yes
-netsh advfirewall firewall add rule name="WinRM open Port 5985" dir=in action=allow protocol=TCP localport=5985
+    Write-Host "Enable WinRM"
+    netsh advfirewall firewall set rule group="remote administration" new enable=yes
+    netsh advfirewall firewall add rule name="WinRM open Port 5985" dir=in action=allow protocol=TCP localport=5985
 
-winrm quickconfig -q
-winrm quickconfig -transport:http
-winrm set winrm/config '@{MaxTimeoutms="7200000"}'
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="0"}'
-winrm set winrm/config/winrs '@{MaxProcessesPerShell="0"}'
-winrm set winrm/config/winrs '@{MaxShellsPerUser="0"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/client/auth '@{Basic="true"}'
+    winrm quickconfig -q
+    winrm quickconfig -transport:http
+    winrm set winrm/config '@{MaxTimeoutms="7200000"}'
+    winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="0"}'
+    winrm set winrm/config/winrs '@{MaxProcessesPerShell="0"}'
+    winrm set winrm/config/winrs '@{MaxShellsPerUser="0"}'
+    winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+    winrm set winrm/config/service/auth '@{Basic="true"}'
+    winrm set winrm/config/client/auth '@{Basic="true"}'
 
-net stop winrm
-sc.exe config winrm start= auto
-net start winrm
-
+    net stop winrm
+    sc.exe config winrm start= auto
+    net start winrm
 }
 
-Get-WmiObject -Class Win32_UserAccount -Filter "name = 'Administrator'" | Set-WmiInstance -Arguments @{PasswordExpires = 0 }
+# Disable pasword expiry for Administrator account
+Get-WmiObject -Class Win32_UserAccount -Filter "name = 'Administrator'" | Set-WmiInstance -Arguments @{PasswordExpires = 0}
+
 Enable-WinRM
