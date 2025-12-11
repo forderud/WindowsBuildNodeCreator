7fe374b38199c8f55055e79c67e649a7939ef6fb
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-12-11 12:58:16 +0100

    Extract ChangeNetworkCategoryToPrivate function.

--- files/bootstrap.ps1
+++ files/bootstrap.ps1
@@ -1,20 +1,21 @@
 # This script is called from the answerfile
 
-# You cannot enable Windows PowerShell Remoting on network connections that are set to Public
-# https://learn.microsoft.com/nb-no/windows/win32/api/netlistmgr/nn-netlistmgr-inetwork
+Function ChangeNetworkCategoryToPrivate {
+    # Cannot enable WinRM on "Public" network connections
+    # https://learn.microsoft.com/nb-no/windows/win32/api/netlistmgr/nn-netlistmgr-inetwork
+    $networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}'))
+    $connections = $networkListManager.GetNetworkConnections()
 
-# Get network connections
-$networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}'))
-$connections = $networkListManager.GetNetworkConnections()
-
-$connections |ForEach-Object {
-    $category = $_.GetNetwork().GetCategory()
-    if ($category -eq 0) { # NLM_NETWORK_CATEGORY_PUBLIC
-        $name = $_.GetNetwork().GetName()
-        Write-Host "Changing $name category from $category to 1"
-        $_.GetNetwork().SetCategory(1) # NLM_NETWORK_CATEGORY_PRIVATE
+    $connections |ForEach-Object {
+        $category = $_.GetNetwork().GetCategory()
+        if ($category -eq 0) { # NLM_NETWORK_CATEGORY_PUBLIC
+            $name = $_.GetNetwork().GetName()
+            Write-Host "Changing $name category from $category to 1"
+            $_.GetNetwork().SetCategory(1) # NLM_NETWORK_CATEGORY_PRIVATE
+        }
     }
 }
+ChangeNetworkCategoryToPrivate
 
 Function Enable-WinRM {
     Write-Host "Enable WinRM"
@@ -28,8 +29,7 @@ Function Enable-WinRM {
     winrm set winrm/config/service/auth '@{Basic="true"}'
     winrm set winrm/config/client/auth '@{Basic="true"}'
 }
+Enable-WinRM
 
 # Disable pasword expiry for Administrator account
 Get-WmiObject -Class Win32_UserAccount -Filter "name = 'Administrator'" | Set-WmiInstance -Arguments @{PasswordExpires = 0}
-
-Enable-WinRM
