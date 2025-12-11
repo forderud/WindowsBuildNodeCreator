3c05c83c08e0a8c2f342387591f21cd5e9f03cbb
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-12-11 01:24:30 +0100

    Cut down firewall setup to _only_ whitelist RDP connections.

--- scripts/phase-1.ps1
+++ scripts/phase-1.ps1
@@ -12,34 +12,9 @@ Import-Module ServerManager
 
 PrintWindowsVersion
 
-Enable-NetFirewallRule -DisplayGroup "Windows Defender Firewall Remote Management" -Verbose
-
-# features and firewall rules common for all Windows Servers
-try {
-    Install-WindowsFeature NET-Framework-45-Core,Telnet-Client,RSAT-Role-Tools -IncludeManagementTools
-    Install-WindowsFeature SNMP-Service,SNMP-WMI-Provider -IncludeManagementTools
-    Enable-NetFirewallRule -DisplayGroup "Remote Desktop" -Verbose
-    Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Verbose
-    Enable-NetFirewallRule -DisplayGroup "Remote Service Management" -Verbose
-    Enable-NetFirewallRule -DisplayGroup "Performance Logs and Alerts" -Verbose
-    Enable-NetFirewallRule -DisplayGroup "Windows Management Instrumentation (WMI)" -Verbose
-    Enable-NetFirewallRule -DisplayGroup "Remote Service Management" -Verbose
-    Enable-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -Verbose
-}
-catch {
-    Write-Output "Phase 1 [ERROR] - setting firewall went wrong"
-}
-
-# Terminal services and sysprep registry entries
-try {
-    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0 -Verbose -Force
-    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 0 -Verbose -Force
-    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0 -Verbose -Force
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\Setup\Status\SysprepStatus' -Name 'GeneralizationState' -Value 7 -Verbose -Force
-}
-catch {
-    Write-Output "Phase 1 [ERROR] - setting registry went wrong"
-}
+# Enable Remote Desktop Protocol (RDP)
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop" -Verbose
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0 -Verbose -Force
 
 # Install chocolatey
 Write-Output "Phase 1 [INFO] - installing Chocolatey, attempt $choco_install_count of $choco_install_count_max"
