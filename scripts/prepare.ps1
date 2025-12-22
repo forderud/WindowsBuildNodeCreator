0b6abdc120889fb7d1bde1125a546ad5b5c576bb
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-12-22 10:23:59 +0100

    Install GEHC root certificates.

--- scripts/prepare.ps1
+++ scripts/prepare.ps1
@@ -2,3 +2,7 @@ $ver = $PSVersionTable.BuildVersion
 Write-Host "Windows OS build: $ver"
 
 Write-Host "PACKER_BUILD_NAME:" $Env:PACKER_BUILD_NAME
+
+Write-Host "Install GEHC Root Certificates..."
+certutil.exe -addstore -f "root" C:\Install\gehealthcarerootca1.crt
+certutil.exe -addstore -f "root" C:\Install\gehealthcarerootca2.crt
