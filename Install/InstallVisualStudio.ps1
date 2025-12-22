8389e0e3830963de61849be97da9f899db93deef
Fredrik Orderud <forderud@gmail.com>
2025-12-22 14:19:04 +0100

    Log VS installation progress to console.

--- Install/InstallVisualStudio.ps1
+++ Install/InstallVisualStudio.ps1
@@ -7,11 +7,11 @@ if ($vsVersion -eq $null) {
     throw "Visual Studio version parameter missing"
 }
 
-# Download Visual Studio bootstrapper
+Write-Host "Downloading Visual Studio bootstrapper..."
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://aka.ms/vs/"+$vsVersion+"/vs_professional.exe","C:\Install\vs_Professional.exe")
 
-# Install Visual Studio with C++ and .Net
+Write-Host "Installing Visual Studio with C++ and .Net..."
 # Component list: https://learn.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-professional
 $process = Start-Process -FilePath "C:\Install\vs_Professional.exe" -ArgumentList `
     "--add", "Microsoft.VisualStudio.Workload.NativeDesktop", `
@@ -28,7 +28,7 @@ if ($process.ExitCode -ne 0) {
 }
 
 if ($vsVersion.Substring(0,2) -eq "17") {
-    # Download and install Windows Driver Kit (WDK) 10.0.22621.2428
+    Write-Host "Downloading and install Windows Driver Kit (WDK) 10.0.22621.2428..."
     $client = new-object System.Net.WebClient
     $client.DownloadFile("https://go.microsoft.com/fwlink/?linkid=2249371","C:\Install\wdksetup.exe")
 
@@ -37,11 +37,11 @@ if ($vsVersion.Substring(0,2) -eq "17") {
         throw "WDK installation failure"
     }
 
-    # Install WDK Visual Studio extension
+    Write-Host "Installing WDK Visual Studio extension..."
     $filePath = Resolve-Path -Path "C:\Program Files (x86)\Windows Kits\10\Vsix\VS2022\*\WDK.vsix"
     $installerPath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\resources\app\ServiceHub\Services\Microsoft.VisualStudio.Setup.Service\VSIXInstaller.exe"
     $process = Start-Process -FilePath $installerPath -ArgumentList @('/quiet', "`"$filePath`"") -Wait -PassThru
 
-    # Set VS170COMNTOOLS env.var. to VS2022 installation folder
+    Write-Host "Set VS170COMNTOOLS env.var. to VS2022 installation folder..."
     [Environment]::SetEnvironmentVariable("VS170COMNTOOLS", "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\", [EnvironmentVariableTarget]::Machine)
 }
