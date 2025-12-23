57ee489cf3515790a56aef146ca47e500c7d082d
Fredrik Orderud <forderud@gmail.com>
2025-12-23 17:41:57 +0100

    Add diagnostics

--- Install/InstallNuGet.ps1
+++ Install/InstallNuGet.ps1
@@ -1,7 +1,7 @@
 # stop script on first error
 $ErrorActionPreference = "Stop"
 
-# Download NuGet.exe
+Write-Host "Downloading NuGet.exe..."
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe","C:\Install\nuget.exe")
 
@@ -21,7 +21,7 @@ if (Test-Path $artifactoryCreds -PathType Leaf) {
     exit 1
 }
 
-# Configure Artifactory authentication
+Write-Host "Configure Artifactory authentication..."
 & "C:\Install\nuget.exe" sources Update -Name nuget-cvus-prod-all -Username $userpw[0] -Password $userpw[1]
 if ($LastExitCode -ne 0) {
     throw "nuget.exe sources Update failure"
@@ -33,12 +33,12 @@ if ($LastExitCode -ne 0) {
     throw "nuget.exe setapikey failure"
 }
 
-# copy nuget to new BuildTools folder
+Write-Host "Copying NuGet.exe to new BuildTools folder..."
 $buildFolder = "C:\BuildTools"
 [void](New-Item $buildFolder -Type Directory)
 Copy-Item "C:\Install\nuget.exe" -Destination $buildFolder
 
-# Add folder to machine-wide PATH
+Write-Host "Add folder to machine-wide PATH..."
 $machinePath = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine)
 $machinePath += ";$buildFolder"
 [Environment]::SetEnvironmentVariable("Path", $machinePath, [EnvironmentVariableTarget]::Machine)
