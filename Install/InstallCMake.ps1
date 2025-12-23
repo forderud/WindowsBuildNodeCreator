7dcb5c8a89353444d381e4bf4aa7d1dc708705e7
Fredrik Orderud <forderud@gmail.com>
2025-12-23 17:41:34 +0100

    Add diagnostics

--- Install/InstallCMake.ps1
+++ Install/InstallCMake.ps1
@@ -1,12 +1,12 @@
 # stop script on first error
 $ErrorActionPreference = "Stop"
 
-# Download CMake 3.29.2 from https://cmake.org/download/
+Write-Host "Downloading CMake 3.29.2 from https://cmake.org/download/ ..."
 $client = new-object System.Net.WebClient
 $cmakeMsiPath = "C:\Install\cmake-windows-x86_64.msi"
 $client.DownloadFile("https://github.com/Kitware/CMake/releases/download/v3.29.2/cmake-3.29.2-windows-x86_64.msi", $cmakeMsiPath)
 
-# Install CMake
+Write-Host "Installing CMake..."
 $process = Start-Process -FilePath msiexec.exe -ArgumentList "/i", $cmakeMsiPath, "/qn", "/norestart" -Wait -PassThru
 if ($process.ExitCode -ne 0) {
     throw "CMake install failure"
