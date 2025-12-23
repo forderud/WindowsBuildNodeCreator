2d54c96999277be42e2c8f700221c936fb7cb800
Fredrik Orderud <forderud@gmail.com>
2025-12-23 20:07:18 +0100

    Add install diagnostics.

--- Install/InstallPython.ps1
+++ Install/InstallPython.ps1
@@ -1,24 +1,24 @@
 # stop script on first error
 $ErrorActionPreference = "Stop"
 
-# Download Python 3.12
+Write-Host "Downloading Python 3.12..."
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe", "C:\Install\python-amd64.exe")
 
-# Install Python
+Write-Host "Installing Python..."
 $process = Start-Process -FilePath "C:\Install\python-amd64.exe" -ArgumentList "/quiet", "InstallAllUsers=1" -Wait -PassThru
 if ($process.ExitCode -ne 0) {
     throw "Python installation failure"
 }
 
-# Enable truststore feature for GEHC CA usage
+Write-Host "Enable truststore feature for GEHC CA usage..."
 # Doc: https://pip.pypa.io/en/stable/topics/https-certificates/
 & "C:\Program Files\Python312\Scripts\pip.exe" config set global.use-feature truststore
 if ($LastExitCode -ne 0) {
     throw "pip truststore failure"
 }
 
-# install python packages
+Write-Host "Installing python packages..."
 & "C:\Program Files\Python312\Scripts\pip.exe" install comtypes numpy matplotlib pywin32
 if ($LastExitCode -ne 0) {
     throw "pip install failure"
