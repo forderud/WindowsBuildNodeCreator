c582e9dd54a513cc139454f448110c47ccab6a1e
Fredrik Orderud <forderud@gmail.com>
2025-12-26 10:51:35 +0100

    Upgrade to Python 3.14.2.

--- Install/InstallPython.ps1
+++ Install/InstallPython.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 
 Write-Host "Downloading Python..."
 $client = new-object System.Net.WebClient
-$client.DownloadFile("https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe", "C:\Install\python-amd64.exe")
+$client.DownloadFile("https://www.python.org/ftp/python/3.14.2/python-3.14.2-amd64.exe", "C:\Install\python-amd64.exe")
 
 Write-Host "Installing Python..."
 $process = Start-Process -FilePath "C:\Install\python-amd64.exe" -ArgumentList "/quiet", "InstallAllUsers=1" -Wait -PassThru
@@ -13,13 +13,13 @@ if ($process.ExitCode -ne 0) {
 
 Write-Host "Enable truststore feature for GEHC CA usage..."
 # Doc: https://pip.pypa.io/en/stable/topics/https-certificates/
-& "C:\Program Files\Python312\Scripts\pip.exe" config set global.use-feature truststore
+& "C:\Program Files\Python314\Scripts\pip.exe" config set global.use-feature truststore
 if ($LastExitCode -ne 0) {
     throw "pip truststore failure"
 }
 
 Write-Host "Installing python packages..."
-& "C:\Program Files\Python312\Scripts\pip.exe" install comtypes numpy matplotlib pywin32
+& "C:\Program Files\Python314\Scripts\pip.exe" install comtypes numpy matplotlib pywin32
 if ($LastExitCode -ne 0) {
     throw "pip install failure"
 }
