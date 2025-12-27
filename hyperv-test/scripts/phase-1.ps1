86a2181261a528104b8c6745973633276c65349a
Fredrik Orderud <forderud@gmail.com>
2025-12-27 14:53:47 +0100

    stop installing chocolatey.

--- hyperv-test/scripts/phase-1.ps1
+++ hyperv-test/scripts/phase-1.ps1
@@ -1,5 +1,5 @@
 # Main Phase-1 script
-# Windows Features, Firewall rules and registry entries,chocolatey
+# Windows Features, Firewall rules
 
 function PrintWindowsVersion {
     $os = Get-WMIObject Win32_OperatingSystem
@@ -15,15 +15,6 @@ PrintWindowsVersion
 # Enable Remote Desktop Protocol (RDP)
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop" -Verbose
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0 -Verbose -Force
-
-# Install chocolatey
-Write-Output "Phase 1 [INFO] - installing Chocolatey, attempt $choco_install_count of $choco_install_count_max"
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
-Write-Output "Phase 1 [INFO] - installing Chocolatey exit code is: $LASTEXITCODE"
-if ($LASTEXITCODE -ne 0) {
-    Write-Output "Phase 1 [ERROR] - Chocolatey install problem, critical, exiting"
-    exit (1)
-}
 
 Write-Output "[INFO] - Setting high performance power plan"
 powercfg.exe /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
