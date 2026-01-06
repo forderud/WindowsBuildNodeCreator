cb1118142507d5a288e23bd823dacf8cc3f81c88
Fredrik Orderud <forderud@gmail.com>
2026-01-06 13:04:06 +0100

    Start on Docker installation script.

---
+++ Install/InstallDocker.ps1
@@ -0,0 +1,13 @@
+# stop script on first error
+$ErrorActionPreference = "Stop"
+
+Write-Host "Downloading Docker..."
+$client = new-object System.Net.WebClient
+$exePath = "C:\Install\DockerDesktopInstaller.exe"
+$client.DownloadFile("https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe", $exePath)
+
+Write-Host "Installing Docker..."
+$process = Start-Process -FilePath $exePath -ArgumentList "install", "--accept-license", "--quiet" -Wait -PassThru
+if ($process.ExitCode -ne 0) {
+    throw "Docker install failure (ExitCode: {0})" -f $process.ExitCode
+}
