fc97c6758df859d9af96af9e3bbc8b9f9f9d3e54
Fredrik Orderud <forderud@gmail.com>
2026-01-06 13:15:10 +0100

    add command-line arg. doc.

--- Install/InstallDocker.ps1
+++ Install/InstallDocker.ps1
@@ -7,6 +7,7 @@ $exePath = "C:\Install\DockerDesktopInstaller.exe"
 $client.DownloadFile("https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe", $exePath)
 
 Write-Host "Installing Docker..."
+# DOC: https://docs.docker.com/desktop/setup/install/windows-install
 $process = Start-Process -FilePath $exePath -ArgumentList "install", "--accept-license", "--quiet" -Wait -PassThru
 if ($process.ExitCode -ne 0) {
     throw "Docker install failure (ExitCode: {0})" -f $process.ExitCode
