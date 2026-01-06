82cd7f4ed6ac142a2c51ef77727ffb45baa7cff6
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-06 16:27:04 +0100

    .

--- Install/InstallNuGet.ps1
+++ Install/InstallNuGet.ps1
@@ -11,9 +11,9 @@ if (Test-Path $artifactoryCreds -PathType Leaf) {
     $password = $creds[1]
     $repoUrl  = $creds[2]
 } else {
+    $repoUrl  = $Env:NUGET_REPO_URL
     $username = $Env:NUGET_REPO_USER
     $password = $Env:NUGET_REPO_PW
-    $repoUrl  = $Env:NUGET_REPO_URL
 }
 
 Write-Host "Downloading NuGet.exe..."
@@ -21,28 +21,30 @@ $client = new-object System.Net.WebClient
 $exePath = "C:\Install\nuget.exe"
 $client.DownloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe", $exePath)
 
-if ($repoUrl) {
+if (-not $repoUrl) {
+    Write-Host "Skipping NuGet repo configuration."
+} else {
     # Add NuGet repo
     $repoName = $repoUrl.Split("/")[-1]
     & $exePath sources Add -Name $repoName -Source $repoUrl
     if ($LastExitCode -ne 0) {
         throw "nuget.exe sources Add failure (ExitCode: {0})" -f $LastExitCode
     }
-}
 
-if ((-not $username) -or (-not $password)) {
-    Write-Host "Skipping NuGet authentication configuration."
-} else {
-    Write-Host "Configure NuGet authentication..."
-    & $exePath sources Update -Name $repoName -Username $username -Password $password
-    if ($LastExitCode -ne 0) {
-        throw "nuget.exe sources Update failure (ExitCode: {0})" -f $LastExitCode
-    }
+    if ((-not $username) -or (-not $password)) {
+        Write-Host "Skipping NuGet authentication configuration."
+    } else {
+        Write-Host "Configure NuGet authentication..."
+        & $exePath sources Update -Name $repoName -Username $username -Password $password
+        if ($LastExitCode -ne 0) {
+            throw "nuget.exe sources Update failure (ExitCode: {0})" -f $LastExitCode
+        }
 
-    $auth = "{0}:{1}" -f $username, $password
-    & $exePath setapikey $auth -Source $repoName
-    if ($LastExitCode -ne 0) {
-        throw "nuget.exe setapikey failure (ExitCode: {0})" -f $LastExitCode
+        $auth = "{0}:{1}" -f $username, $password
+        & $exePath setapikey $auth -Source $repoName
+        if ($LastExitCode -ne 0) {
+            throw "nuget.exe setapikey failure (ExitCode: {0})" -f $LastExitCode
+        }
     }
 }
 
