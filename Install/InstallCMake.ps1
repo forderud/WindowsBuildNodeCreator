954efcfc2f90566f6377503e2f156303da60403b
Fredrik Orderud <forderud@gmail.com>
2026-01-06 14:04:04 +0100

    .

--- Install/InstallCMake.ps1
+++ Install/InstallCMake.ps1
@@ -4,11 +4,11 @@ $ErrorActionPreference = "Stop"
 # Download page: https://cmake.org/download/
 Write-Host "Downloading CMake..."
 $client = new-object System.Net.WebClient
-$cmakeMsiPath = "C:\Install\cmake-windows-x86_64.msi"
-$client.DownloadFile("https://github.com/Kitware/CMake/releases/download/v4.2.1/cmake-4.2.1-windows-x86_64.msi", $cmakeMsiPath)
+$msiPath = "C:\Install\cmake-windows-x86_64.msi"
+$client.DownloadFile("https://github.com/Kitware/CMake/releases/download/v4.2.1/cmake-4.2.1-windows-x86_64.msi", $msiPath)
 
 Write-Host "Installing CMake..."
-$process = Start-Process -FilePath msiexec.exe -ArgumentList "/i", $cmakeMsiPath, "/qn", "/norestart" -Wait -PassThru
+$process = Start-Process -FilePath msiexec.exe -ArgumentList "/i", $msiPath, "/qn", "/norestart" -Wait -PassThru
 if ($process.ExitCode -ne 0) {
     throw "CMake install failure (ExitCode: {0})" -f $process.ExitCode
 }
