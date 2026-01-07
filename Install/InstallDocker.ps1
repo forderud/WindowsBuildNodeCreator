9f47b3fe16be9a0850a13668d14686bdffcf3197
Fredrik Orderud <forderud@gmail.com>
2026-01-07 18:42:24 +0100

    Switch to Docker Community Edition (Docker CE) to work around AWS installation problems.

--- Install/InstallDocker.ps1
+++ Install/InstallDocker.ps1
@@ -1,17 +1,10 @@
 # stop script on first error
 $ErrorActionPreference = "Stop"
 
-Write-Host "Downloading Docker..."
-$client = new-object System.Net.WebClient
-$exePath = "C:\Install\DockerDesktopInstaller.exe"
-$client.DownloadFile("https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe", $exePath)
+# DOC: https://learn.microsoft.com/en-us/virtualization/windowscontainers/quick-start/set-up-environment
 
-Write-Host "Installing Docker..."
-# DOC: https://docs.docker.com/desktop/setup/install/windows-install
-$process = Start-Process -FilePath $exePath -ArgumentList "install", "--accept-license", "--quiet" -Wait -PassThru
-if ($process.ExitCode -ne 0) {
-    # Installation log on C:\Users\Administrator\AppData\Local\Docker\log\host\DockerDesktopInstaller.exe.log
-    #Write-Host("Docker install failure (ExitCode: {0})" -f $process.ExitCode)
-    #sleep 600 # sleep 10min to get a chance to debug
-    throw "Docker install failure (ExitCode: {0})" -f $process.ExitCode
-}
+Write-Host "Downloading Docker Community Edition (CE) installation script..."
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
+
+Write-Host "Installing Docker CE..."
+.\install-docker-ce.ps1
