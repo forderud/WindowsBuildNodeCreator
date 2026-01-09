317e35714145e4d25abcdb43cbd5005a9103b1ae
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-09 13:30:13 +0100

    Add commented-out crash dump collection commands.

--- scripts/prepare.ps1
+++ scripts/prepare.ps1
@@ -8,3 +8,6 @@ Write-Host "Temporary Administrator password: $Env:WINRMPASS"
 Write-Host "Install GEHC Root Certificates..."
 certutil.exe -addstore -f "root" C:\Install\gehealthcarerootca1.crt
 certutil.exe -addstore -f "root" C:\Install\gehealthcarerootca2.crt
+
+#Write-Host "Enable crash dump collection to %LOCALAPPDATA%\CrashDumps ..."
+#New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps"
