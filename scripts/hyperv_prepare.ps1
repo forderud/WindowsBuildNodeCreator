d49fa7a4dbf4512554ee9f94598f3048fb79cd93
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-13 19:38:23 +0100

    Remove redundant Windows version logging that's also done in prepare.ps1.

--- scripts/hyperv_prepare.ps1
+++ scripts/hyperv_prepare.ps1
@@ -1,23 +1,6 @@
-# Main Phase-1 script
-# Windows Features, Firewall rules
-
-function PrintWindowsVersion {
-    $os = Get-WMIObject Win32_OperatingSystem
-    Write-Output "Phase 1 [INFO] - $($os.Caption) (version $($os.Version)) found."
-}
-
-# Phase 1 - Mandatory generic stuff
-Write-Output "Phase 1 [START] - Start of Phase 1"
-Import-Module ServerManager
-
-PrintWindowsVersion
-
-# Enable Remote Desktop Protocol (RDP)
+Write-Output "Enable Remote Desktop Protocol (RDP).."
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop" -Verbose
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0 -Verbose -Force
 
-Write-Output "[INFO] - Setting high performance power plan"
+Write-Output "Setting high performance power plan..."
 powercfg.exe /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
-
-Write-Output "Phase 1 [END] - End of Phase 1"
-exit 0
