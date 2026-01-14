5064a1919738305abde24b46a215c84d79604d98
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-14 21:17:21 +0100

    .

--- scripts/hyperv_shutdown.bat
+++ scripts/hyperv_shutdown.bat
@@ -1,7 +1,7 @@
 :: Stop whitelisting WinRM in firewall
 ::netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new enable=no
 
+:: TODO: Figure out why sysprep causes connection failure
 ::C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /unattend:C:\Windows\System32\Sysprep\unattend.xml /quiet /shutdown
 
 shutdown /s /t 10 /f /d p:4:1 /c Packer_Provisioning_Shutdown
-::pause
