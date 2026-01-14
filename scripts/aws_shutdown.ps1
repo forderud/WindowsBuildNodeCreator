78d4a126742a991483d3de9ee33f695ae703d042
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-14 14:20:08 +0100

    Also log that RDP is disabled.

--- scripts/aws_shutdown.ps1
+++ scripts/aws_shutdown.ps1
@@ -5,5 +5,5 @@ Write-Host "Reset admin password..."
 & 'C:/Program Files/Amazon/EC2Launch/ec2launch' reset
 
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/sysprep-using-ec2launchv2.html
-Write-Host "Make image generic..."
+Write-Host "Disable RDP, then call sysprep to generalize image..."
 & 'C:/Program Files/Amazon/EC2Launch/ec2launch' sysprep --shutdown
