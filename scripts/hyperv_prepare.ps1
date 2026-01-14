e2d1627cd5eead5ad83666b6d881a3e4845f7199
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-14 10:52:54 +0100

    .

--- scripts/hyperv_prepare.ps1
+++ scripts/hyperv_prepare.ps1
@@ -2,5 +2,5 @@ Write-Output "Enable Remote Desktop Protocol (RDP).."
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop" -Verbose
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0 -Verbose -Force
 
-Write-Output "Setting high performance power plan..."
+Write-Output "Switch to High Performance power plan to disable automatic sleep..."
 powercfg.exe /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
