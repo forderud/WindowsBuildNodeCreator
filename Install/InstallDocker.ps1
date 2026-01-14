a81050b42006979ee60efa5f792f3a6baccd8a36
Fredrik Orderud <forderud@gmail.com>
2026-01-14 09:17:56 +0100

    Force restart to avoid the following error on Hyper-V builds: The system shutdown cannot be initiated because there are other users logged on to the computer.

--- Install/InstallDocker.ps1
+++ Install/InstallDocker.ps1
@@ -7,4 +7,6 @@ Write-Host "Downloading Docker Community Edition (CE) installation script..."
 Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
 
 Write-Host "Installing Docker CE..."
-.\install-docker-ce.ps1
+# Force restart to avoid the following error on Hyper-V builds:
+# The system shutdown cannot be initiated because there are other users logged on to the computer.
+.\install-docker-ce.ps1 -Force
