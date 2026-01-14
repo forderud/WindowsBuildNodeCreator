3c713ea287c3c3edb388308bc7b944aecd9a4aeb
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-14 14:13:02 +0100

    Add ec2launch sysprep doc link

--- scripts/aws_shutdown.ps1
+++ scripts/aws_shutdown.ps1
@@ -4,5 +4,6 @@ Set-Service -Name WinRM -StartupType Disabled
 Write-Host "Reset admin password..."
 & 'C:/Program Files/Amazon/EC2Launch/ec2launch' reset
 
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/sysprep-using-ec2launchv2.html
 Write-Host "Make image generic..."
 & 'C:/Program Files/Amazon/EC2Launch/ec2launch' sysprep --shutdown
