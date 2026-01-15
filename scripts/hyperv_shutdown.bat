e1642ae34c2aaab595f99b68c888f3d8327d09cc
Fredrik Orderud <forderud@gmail.com>
2026-01-15 13:06:48 +0100

    Call sysprep also for Hyper-V builds

--- scripts/hyperv_shutdown.bat
+++ scripts/hyperv_shutdown.bat
@@ -1,7 +1,5 @@
 :: Stop whitelisting WinRM in firewall
 ::netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new enable=no
 
-:: TODO: Figure out why sysprep causes connection failure
-::"C:\Windows\System32\Sysprep\Sysprep.exe" /generalize /oobe /unattend:E:\unattend.xml /quiet /shutdown
-
-shutdown /s /t 10 /f /d p:4:1 /c Packer_Provisioning_Shutdown
+:: Call sysprep to generalize image
+"C:\Windows\System32\Sysprep\Sysprep.exe" /generalize /oobe /unattend:E:\unattend.xml /quiet /shutdown
