d98e0a1911ff623ff8ecba1a00d7e7fb44cbc33e
Fredrik Orderud <forderud@gmail.com>
2026-01-19 00:56:09 +0100

    .

--- scripts/hyperv_shutdown.bat
+++ scripts/hyperv_shutdown.bat
@@ -2,6 +2,6 @@
 ::netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new enable=no
 
 :: TODO: Figure out why sysprep causes connection failure
-::C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /unattend:E:\unattend.xml /quiet /shutdown
+::"C:\Windows\System32\Sysprep\Sysprep.exe" /generalize /oobe /unattend:E:\unattend.xml /quiet /shutdown
 
 shutdown /s /t 10 /f /d p:4:1 /c Packer_Provisioning_Shutdown
