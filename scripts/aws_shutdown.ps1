e55f66274a798d8bd510dce57bd35cda42aa5cc0
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-19 13:48:20 +0100

    .

--- scripts/aws_shutdown.ps1
+++ scripts/aws_shutdown.ps1
@@ -5,6 +5,7 @@ Write-Host "Reset admin password..."
 & "C:/Program Files/Amazon/EC2Launch/ec2launch" reset
 
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/sysprep-using-ec2launchv2.html
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2launch-v2-settings.html
 Write-Host "Disable RDP, then call sysprep to generalize image..."
 & "C:/Program Files/Amazon/EC2Launch/ec2launch.exe" sysprep
 
