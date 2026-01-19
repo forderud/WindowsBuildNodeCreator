2d7713b23ff5f98e38b05f9ddfc2d6ae5630242f
Fredrik Orderud <forderud@gmail.com>
2026-01-19 12:30:45 +0100

    .

--- scripts/aws_shutdown.ps1
+++ scripts/aws_shutdown.ps1
@@ -2,11 +2,11 @@ Write-Host "Disable WinRM startup on next boot..."
 Set-Service -Name WinRM -StartupType Disabled
 
 Write-Host "Reset admin password..."
-& 'C:/Program Files/Amazon/EC2Launch/ec2launch' reset
+& "C:/Program Files/Amazon/EC2Launch/ec2launch" reset
 
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/sysprep-using-ec2launchv2.html
 Write-Host "Disable RDP, then call sysprep to generalize image..."
-& 'C:/Program Files/Amazon/EC2Launch/ec2launch.exe' sysprep
+& "C:/Program Files/Amazon/EC2Launch/ec2launch.exe" sysprep
 
 Write-Host "Shutting down..."
 & shutdown /s /t 10 /f /d p:4:1 /c Packer_Provisioning_Shutdown
