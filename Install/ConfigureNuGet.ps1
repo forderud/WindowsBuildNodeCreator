51314cfb8c499add62da70bb165221b1b98dfa74
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-26 14:06:41 +0100

    Split machine-wide nuget installation from per-user nuget configuration. Also, stop copying nuget.config to "system" account, since it doesn't seem to work.

---
+++ Install/ConfigureNuGet.ps1
@@ -0,0 +1,39 @@
+# stop script on first error
+$ErrorActionPreference = "Stop"
+
+# NOTICE: The script expects $Env:NUGET_REPO_USER and $Env:NUGET_REPO_PW to have already been set
+$repoUrl  = $Env:NUGET_REPO_URL
+$username = $Env:NUGET_REPO_USER
+$password = $Env:NUGET_REPO_PW
+
+Write-Host "NuGet repo URL: $repoUrl"
+Write-Host "NuGet username: $username"
+Write-Host "NuGet password: $password"
+
+# Add NuGet repo
+$exePath = "C:\Install\nuget.exe"
+$repoName = $repoUrl.Split("/")[-1]
+& $exePath sources Add -Name $repoName -Source $repoUrl
+if ($LastExitCode -ne 0) {
+    throw "nuget.exe sources Add failure (ExitCode: {0})" -f $LastExitCode
+}
+
+if ((-not $username) -or (-not $password)) {
+    Write-Host "SKIPPING NuGet authentication configuration."
+} else {
+    Write-Host "Configure NuGet authentication..."
+    # Pass -StorePasswordInClearText to avoid "CryptographicException: Access is denied" errors on AWS
+    & $exePath sources Update -Name $repoName -Username $username -Password $password -Verbosity detailed
+    if ($LastExitCode -ne 0) {
+        #Write-Host("nuget.exe sources Update failure (ExitCode: {0})" -f $LastExitCode)
+        #sleep 1800 # sleep 30min to give time for interactive debugging
+        throw "nuget.exe sources Update failure (ExitCode: {0})" -f $LastExitCode
+    }
+
+    # Gives "CryptographicException: Access is denied" errors on AWS
+    $auth = "{0}:{1}" -f $username, $password
+    & $exePath setapikey $auth -Source $repoName -Verbosity detailed
+    if ($LastExitCode -ne 0) {
+        throw "nuget.exe setapikey failure (ExitCode: {0})" -f $LastExitCode
+    }
+}
--- Install/InstallNuGet.ps1
+++ Install/InstallNuGet.ps1
@@ -1,58 +1,10 @@
 # stop script on first error
 $ErrorActionPreference = "Stop"
 
-# NOTICE: The script expects $Env:NUGET_REPO_USER and $Env:NUGET_REPO_PW to have already been set
-$repoUrl  = $Env:NUGET_REPO_URL
-$username = $Env:NUGET_REPO_USER
-$password = $Env:NUGET_REPO_PW
-
-Write-Host "NuGet repo URL: $repoUrl"
-Write-Host "NuGet username: $username"
-Write-Host "NuGet password: $password"
-
 Write-Host "Downloading NuGet.exe..."
 $client = new-object System.Net.WebClient
 $exePath = "C:\Install\nuget.exe"
 $client.DownloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe", $exePath)
-
-if (-not $repoUrl) {
-    Write-Host "SKIPPING NuGet repo configuration."
-} else {
-    # Add NuGet repo
-    $repoName = $repoUrl.Split("/")[-1]
-    & $exePath sources Add -Name $repoName -Source $repoUrl
-    if ($LastExitCode -ne 0) {
-        throw "nuget.exe sources Add failure (ExitCode: {0})" -f $LastExitCode
-    }
-
-    if ((-not $username) -or (-not $password)) {
-        Write-Host "SKIPPING NuGet authentication configuration."
-    } else {
-        Write-Host "Configure NuGet authentication..."
-        # Pass -StorePasswordInClearText to avoid "CryptographicException: Access is denied" errors on AWS
-        & $exePath sources Update -Name $repoName -Username $username -Password $password -Verbosity detailed
-        if ($LastExitCode -ne 0) {
-            #Write-Host("nuget.exe sources Update failure (ExitCode: {0})" -f $LastExitCode)
-            #sleep 1800 # sleep 30min to give time for interactive debugging
-            throw "nuget.exe sources Update failure (ExitCode: {0})" -f $LastExitCode
-        }
-
-        # Gives "CryptographicException: Access is denied" errors on AWS
-        $auth = "{0}:{1}" -f $username, $password
-        & $exePath setapikey $auth -Source $repoName -Verbosity detailed
-        if ($LastExitCode -ne 0) {
-            throw "nuget.exe setapikey failure (ExitCode: {0})" -f $LastExitCode
-        }
-    }
-
-    # Copy NuGet config file to the SYSTEM account
-    $src = "$Env:APPDATA\NuGet"
-    $dst = "C:\Windows\System32\config\systemprofile\AppData\Roaming\NuGet"
-    if (-not (Test-Path $dst -PathType Container)) {
-        [void](New-Item $dst -Type Directory)
-    }
-    Copy-Item "$src\NuGet.Config" -Destination "$dst\NuGet.Config"
-}
 
 Write-Host "Copying NuGet.exe to new BuildTools folder..."
 $buildFolder = "C:\BuildTools"
