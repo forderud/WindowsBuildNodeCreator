7ad690f5781db357219e2fa5437e7e209cd15862
Fredrik Orderud <forderud@gmail.com>
2026-02-01 23:26:07 +0100

    Test -ExecutionPolicy Bypass modification.

--- Install/InstallDocker.ps1
+++ Install/InstallDocker.ps1
@@ -4,7 +4,11 @@ $ErrorActionPreference = "Stop"
 # DOC: https://learn.microsoft.com/en-us/virtualization/windowscontainers/quick-start/set-up-environment
 
 Write-Host "Downloading Docker Community Edition (CE) installation script..."
-Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
+
+#$scriptUrl = "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" # upstream version
+$scriptUrl = "https://raw.githubusercontent.com/forderud/Windows-Containers/refs/heads/ExecutionPolicy/helpful_tools/Install-DockerCE/install-docker-ce.ps1" # ExecutionPolicy modification
+
+Invoke-WebRequest -UseBasicParsing $scriptUrl -o install-docker-ce.ps1
 
 Write-Host "Installing Docker CE..."
 # Force restart to avoid the following error on Hyper-V builds:
