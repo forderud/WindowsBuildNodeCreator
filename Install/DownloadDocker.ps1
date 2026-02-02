dfc143f6874528837b3d5d7294191e487ad5b32b
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-02 10:17:38 +0100

    .

--- Install/DownloadDocker.ps1
+++ Install/DownloadDocker.ps1
@@ -2,10 +2,8 @@
 $ErrorActionPreference = "Stop"
 
 # DOC: https://learn.microsoft.com/en-us/virtualization/windowscontainers/quick-start/set-up-environment
-
 Write-Host "Downloading Docker Community Edition (CE) installation script..."
 
-$scriptUrl = "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" # upstream version
-#$scriptUrl = "https://raw.githubusercontent.com/forderud/Windows-Containers/refs/heads/ExecutionPolicy/helpful_tools/Install-DockerCE/install-docker-ce.ps1" # ExecutionPolicy modification
-
+# Download script from https://github.com/microsoft/Windows-Containers
+$scriptUrl = "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1"
 Invoke-WebRequest -UseBasicParsing $scriptUrl -o "C:\Install\install-docker-ce.ps1"
