79f3cc2b3d48613243ea22b6e9fbd1ffeb905476
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-05 17:02:09 +0100

    .

--- Install/CreateCleanJenkinsWorkspaceTask.ps1
+++ Install/CreateCleanJenkinsWorkspaceTask.ps1
@@ -10,7 +10,7 @@ $trigger = New-ScheduledTaskTrigger -Daily -At 3am
 # only run if idle for 5min (wait up to 3hours)
 $settings = New-ScheduledTaskSettingsSet -RunOnlyIfIdle -IdleDuration 00:05:00 -IdleWaitTimeout 03:00:00
 
-Register-ScheduledTask -Action $action -User "System" -Trigger $trigger -Settings $settings -TaskName "Jenkins cleanup" -Description "Delete old  Jenkins workspaces"
+Register-ScheduledTask -Action $action -User "System" -Trigger $trigger -Settings $settings -TaskName "Jenkins cleanup" -Description "Delete old Jenkins workspaces"
 
 Write-Host "Excluding Jenkins workspace from antivirus scans..."
 Add-MpPreference -ExclusionPath $jeninsWorkspace
